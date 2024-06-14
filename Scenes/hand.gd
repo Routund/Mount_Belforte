408f703fa133d014e07bc9d93c8e6097aa87787a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-14 12:40:41 +1200

    Worked on cards having fluid motion between hand and queue

--- Scenes/hand.gd
+++ Scenes/hand.gd
@@ -7,9 +7,14 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func animate():
+	var tween = create_tween()
+	for sprite in get_children():
+		sprite.animate_self()
 	pass
 
 func _on_button_confirm_play():
-	for i in get_children():
-		i.queue_free()
+	var tween = create_tween()
+	for sprite in get_children():
+		sprite.animate_self()
+	pass
--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -3,33 +3,62 @@ extends TextureButton
 @export var card_id=0
 var queue_pos=0
 var queued = false
-
-
+var previous_pos = Vector2(0,-7)
 var battle_manager
-
+var animate = false
+var hand = self
+var queue = self
 @onready var text = get_node("Label")
 
 var is_button_pressed = false
 func _ready():
 	text.text=str(card_id)
 	battle_manager=get_parent().get_parent()
+	previous_pos = global_position
+	animate_self()
+	queue = battle_manager.get_node("Queue")
+	hand = battle_manager.get_node("Hand")
 	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if animate:
+		animate =false
+
+
 	if is_button_pressed and not is_pressed():
 		is_button_pressed = false
 		var new_parent=self
+
 		if queued:
-			new_parent = battle_manager.get_node("Hand")
+			new_parent = hand
 			battle_manager.dequeue_card(card_id)
 		else:
-			new_parent = battle_manager.get_node("Queue")
+			new_parent = queue
 			battle_manager.queue_card(card_id)
 		queued=!queued
+		animate=true
 		get_parent().remove_child(self)
 		new_parent.add_child(self)
+		hand.animate()
+		queue.animate()
 		
 	elif is_pressed() and not is_button_pressed:
 		# Button is pressed for the first time
 		is_button_pressed = true
+
+func animate_self():
+	print(card_id)
+	print(global_position)
+	var newTransform = global_position
+	var tween = create_tween()
+	global_position=previous_pos
+	tween.tween_property(self, "global_position", newTransform, 1)
+	tween.connect("finished", on_tween_finished)
+
+func on_tween_finished():
+	previous_pos=global_position
+	print(card_id)
+	print(previous_pos)
+	
+	
