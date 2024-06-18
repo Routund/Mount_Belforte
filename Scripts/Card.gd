693f0305984d722c35c041aedf8fd8494be222a0
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-18 14:15:37 +1200

    Worked on more permanent fix to card flicker

--- Scripts/Card.gd
+++ Scripts/Card.gd
@@ -8,16 +8,24 @@ var battle_manager
 var animate = false
 var hand = self
 var queue = self
+var fakeH = self
+var fakeQ = self
 @onready var text = get_node("Label")
+var fake_preload = preload("res://Scenes/fake_card.tscn")
+var fake = self
 
 var is_button_pressed = false
 func _ready():
 	text.text=str(card_id)
 	battle_manager=get_parent().get_parent()
-	previous_pos = global_position-Vector2(0,-500)
 	queue = battle_manager.get_node("Queue")
 	hand = battle_manager.get_node("Hand")
-	animate=true
+	fakeQ = battle_manager.get_node("FakeQ")
+	fakeH = battle_manager.get_node("FakeH")
+	fake = fake_preload.instantiate()
+	fakeH.add_child(fake)
+	previous_pos=global_position-Vector2(0,-100)
+	animate = true
 	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,36 +38,34 @@ func _process(_delta):
 	if is_button_pressed and not is_pressed():
 		is_button_pressed = false
 		var new_parent=self
-
+		var new_fake_parent= self
 		if queued:
 			new_parent = hand
+			new_fake_parent = fakeH
 			battle_manager.dequeue_card(card_id)
 		else:
 			new_parent = queue
+			new_fake_parent = fakeQ
 			battle_manager.queue_card(card_id)
 		queued=!queued
 		animate=true
-		get_parent().remove_child(self)
-		new_parent.add_child(self)
+		fake.get_parent().remove_child(fake)
+		new_fake_parent.add_child(fake)
+		reparent(new_parent,true)
 		
 	elif is_pressed() and not is_button_pressed:
 		# Button is pressed for the first time
 		is_button_pressed = true
 
 func animate_self():
-	print(card_id)
-	print(global_position)
-	var newTransform = global_position
+	var newTransform = fake.global_position
 	var tween = create_tween()
-	global_position=previous_pos
-	tween.tween_property(self, "global_position", newTransform, 0.04)
+	tween.tween_property(self, "global_position", newTransform, 0.1)
 	tween.connect("finished", on_tween_finished)
 	disabled = true
 
 func on_tween_finished():
 	previous_pos=global_position
-	print(card_id)
-	print(previous_pos)
 	disabled = false
 	
 	
