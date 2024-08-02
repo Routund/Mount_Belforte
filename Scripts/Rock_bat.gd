cf8aa37280aab90b77896b86a2909c33ee3bf60c
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-08-02 12:37:57 +1200

    enemy vision

--- Scripts/Rock_bat.gd
+++ Scripts/Rock_bat.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 # Declare member variables here. Examples:
 # var a = 2
-
+var seen_player = false
 var rock = false
+<<<<<<< Updated upstream
 var speed = 600
 var rock_speed = 700
+=======
+var speed = 400
+var rock_speed = 400
+var direction = Vector2.ZERO
+>>>>>>> Stashed changes
 @onready var player = get_parent().get_parent().get_node("Player")
 
 var movement_speed: float = 400.0
@@ -18,7 +24,6 @@ func _ready():
 	$AnimationPlayer.play("walk")
 	navigation_agent.path_desired_distance = 4.0
 	navigation_agent.target_desired_distance = 4.0
-# Make sure to not await during _ready.
 	call_deferred("actor_setup")
 
 func actor_setup():
@@ -30,7 +35,7 @@ func set_movement_target(_movement_target: Vector2):
 		navigation_agent.target_position = player.global_position
 	
 func _physics_process(_delta):
-	if rock == false:
+	if rock == false and seen_player == true:
 		navigation_agent.target_position = player.global_position
 		if navigation_agent.is_navigation_finished():
 			return
@@ -39,8 +44,7 @@ func _physics_process(_delta):
 		var new_velocity: Vector2 = next_path_position - current_agent_position
 		new_velocity = new_velocity.normalized()
 		velocity = new_velocity * speed
-	else:
-		var direction = Vector2.ZERO
+	elif rock == true:
 		if $down.is_colliding() and $down.get_collider().name == "Player":
 			direction += Vector2(0,-1)
 		elif $up.is_colliding() and $up.get_collider().name == "Player":
@@ -62,9 +66,22 @@ func _physics_process(_delta):
 				rock_speed = -700
 				$Rock_collision.disabled = true
 			else:
-				rock_speed = 700
+				rock_speed = 900
 				$Rock_collision.disabled = false
 		velocity = direction * rock_speed
+	elif seen_player == false:
+		if $down.is_colliding():
+			direction = Vector2(0,-1)
+		elif $up.is_colliding():
+			direction = Vector2(0,1)
+		elif $left.is_colliding():
+			direction = Vector2(1,0)
+		elif $right.is_colliding():
+			direction = Vector2(-1,0)
+		if direction == Vector2(0,0):
+			direction = Vector2(-1,0)
+		velocity = direction * speed
+			
 	move_and_slide()
 
 
@@ -73,5 +90,18 @@ func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		Global.battle(0)
 		rock = true
+<<<<<<< Updated upstream
 		$rock.show()
 		$Sprite2D.hide()
+=======
+		Global.battle(1)
+
+func give_coords():
+	Global.state_dictionary["bat_pos"]=position
+	Global.state_dictionary["rock_state"]=rock
+
+
+func _on_vision_body_entered(body):
+	if body.name == "Player":
+		seen_player = true
+>>>>>>> Stashed changes
--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 # Declare member variables here. Examples:
 # var a = 2
+var seen_player = false
 var death = false
 var speed = 600
 @onready var player = get_parent().get_parent().get_node("Player")
@@ -20,15 +21,18 @@ func _ready():
 	call_deferred("actor_setup")
 
 func actor_setup():
-	# Wait for the first physics frame so the NavigationServer can sync.
-	await get_tree().physics_frame
-	set_movement_target(_movement_target_position)
+	if seen_player == true:
+		await get_tree().physics_frame
+		set_movement_target(_movement_target_position)
 func set_movement_target(_movement_target: Vector2):
 	navigation_agent.target_position = player.global_position
 	
 func _physics_process(_delta):
-	if death == false:
+	if death == false and seen_player == true:
 		navigation_agent.target_position = player.global_position
+	elif seen_player == false:
+		pass
+	
 	if navigation_agent.is_navigation_finished():
 		return
 	var current_agent_position: Vector2 = global_position
@@ -37,6 +41,7 @@ func _physics_process(_delta):
 	new_velocity = new_velocity.normalized()
 	velocity = new_velocity * speed
 	move_and_slide()
+	
 
 
 
@@ -46,6 +51,7 @@ func _on_area_2d_body_entered(body):
 		if death == false:
 			var rng = RandomNumberGenerator.new()
 			var get_water = rng.randi_range(1,3)
+<<<<<<< Updated upstream
 			if 5 not in Global.inventory and get_water == 4: #make it three when water bottle works 
 				$card.show()
 				$Sprite2D.hide()
@@ -54,6 +60,19 @@ func _on_area_2d_body_entered(body):
 				queue_free()
 		elif death == true:
 			Global.slime += 1
+=======
+			if 5 not in Global.inventory and get_water == 1:
+				water_card = true
+				Global.state_dictionary["water"]=water_card
+			Global.battle(0)
+		elif water_card == true:
+			Global.slime -= 1
+>>>>>>> Stashed changes
 			Global.inventory.append(5)
 			queue_free()
 		
+
+
+func _on_vision_body_entered(body):
+	if body.name == "Player":
+		seen_player = true
